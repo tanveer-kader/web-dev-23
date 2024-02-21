de7c5bcd56e1592125008443e902b032d6714f36
tanveer-kader <tanveerkaderedu@gmail.com>
2024-02-21 23:22:05 +0600

    perf(validation): forms validation and error handling

---
+++ 43-YelpCamp-Validation-Data/yelpcamp/seeds/index.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
+  // useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui neque natus dignissimos! Praesentium nam rerum, dolores libero obcaecati voluptatum. A atque omnis, perspiciatis ut beatae libero laboriosam modi minus error?",
+      price,
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
